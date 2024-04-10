941e3fb0bd8c5511ba312913ec489af556bba073
mcarthur <mcarthur.alford@proton.me>
2024-04-10 07:44:42 +0000

    Removed unused display module

--- crates/burn-derive/src/module/mod.rs
+++ crates/burn-derive/src/module/mod.rs
@@ -1,7 +1,6 @@
 pub(crate) mod codegen;
 pub(crate) mod codegen_enum;
 pub(crate) mod codegen_struct;
-pub(crate) mod display;
 pub(crate) mod record;
 pub(crate) mod record_enum;
 pub(crate) mod record_struct;
